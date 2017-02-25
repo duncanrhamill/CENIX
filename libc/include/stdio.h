71ea413da6d22ade32d5e0b5a83a49660be92ab9
Duncan R Hamill <duncanrhamill@googlemail.com>
2017-02-25 18:18:21 +0000

    added multiboot and updated makefil

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -12,6 +12,8 @@ extern "C" {
     int printf(const char* __restrict, ...);
     int putchar(int);
     int puts(const char*);
+    char* itoa(int, char*, int);
+    char* ltoa(long, char*, int);
 
 #ifdef __cplusplus
 }
